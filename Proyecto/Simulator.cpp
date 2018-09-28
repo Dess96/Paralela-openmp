4178945b4820a0d0cad8fa08b60c4e2001012591
Dess96 <dessalfaro@gmail.com>
2018-09-28 15:56:58 -0600

    Version 14
    *Inicializar paralelizado y funcionando
    *Escribir sobre el archivo, hacerle resize a la matriz y cout de archivo generado hechos single
    *Hacer for de listas paralelizables

--- Proyecto/Simulator.cpp
+++ Proyecto/Simulator.cpp
@@ -3,6 +3,7 @@
 #include<iostream>
 #include<random>
 #include<time.h>
+#include<omp.h>
 #include <fstream>
 #include<stdlib.h>
 
@@ -15,7 +16,7 @@ using namespace std;
 * infectiousness: Potencia infecciosan
 * chance_recover: Probabilidad de recuperacion
 * infected: Personas infectadas inicialmente*/
-int world_size, death_duration, tic;
+int world_size, death_duration, tic, thread_count;
 double infected, infectiousness, chance_recover, number_people;
 
 int Simulator::initialize(int number_peopleM, double infectiousnessM, double chance_recoverM, int death_durationM, double infectedM, int world_sizeM, int ticM) {
@@ -26,32 +27,46 @@ int Simulator::initialize(int number_peopleM, double infectiousnessM, double cha
 	infected = infectedM;
 	world_size = world_sizeM;
 	tic = ticM;
+	thread_count = omp_get_num_threads();
 	Person p;
-	v.resize(world_size); //Vector de vectores de tamaño world_size*world_size
-	world.resize(world_size, v);
+#pragma omp single
+	{
+		v.resize(world_size); //Vector de vectores de tamaño world_size*world_size
+		world.resize(world_size, v);
+	}
 	int perc;
 	int healthy;
 	int pos1, pos2;
 	perc = number_people * infected / 100; //Cantidad correspondiente al porcentaje dado
 	healthy = number_people - perc; //Gente sana
 	srand(time(NULL));
+#pragma omp parallel for num_threads (thread_count)
 	for (int i = 0; i < perc; i++) { //Cambiamos a los infectados
 		p.create(); //Se crean sanos y con su x y y
 		pos1 = rand() % world_size;
 		pos2 = rand() % world_size;
-		p.setX(pos1);
-		p.setY(pos2);
-		p.change_state(1);
+#pragma omp critical
+		{
+			p.setX(pos1);
+			p.setY(pos2);
+			p.change_state(1);
+		}
+#pragma omp atomic
 		world[pos1][pos2]++; //Metemos a la persona en la lista de la posicion correspondiente	
 		lists.push_back(p);
 	}
+#pragma omp parallel for num_threads (thread_count)
 	for (int i = 0; i < healthy; i++) {
 		p.create();
 		pos1 = rand() % world_size;
 		pos2 = rand() % world_size;
+#pragma omp critical
+		{
 		p.setX(pos1);
 		p.setY(pos2);
 		p.change_state(0);
+		}
+#pragma omp atomic
 		world[pos1][pos2]++;
 		lists.push_back(p);
 	}
@@ -158,9 +173,12 @@ void Simulator::update(string name, int healthy_people) {
 		}
 		clear(actual_tic, name, healthy_people, dead_people, sick_people, inmune_people);
 	}
+#pragma omp single
+	{
 	cout << "Archivo generado" << endl;
 	lists.clear();
 	world.clear();
+	}
 }
 
 int Simulator::movePos(int pos, int world_size) {
@@ -180,15 +198,18 @@ int Simulator::movePos(int pos, int world_size) {
 void Simulator::clear(int actual_tic, string name, int healthy_people, int dead_people, int sick_people, int inmune_people) {
 	int i, j, state;
 	list<Person>::iterator it = lists.begin();
-	ofstream file;
-	file.open(name, ios_base::app);
-	file << "Reporte del tic " << actual_tic << endl
-		<< " Personas muertas total " << dead_people << ", promedio " << dead_people / actual_tic << ", porcentaje " << number_people * dead_people / 100 << endl
-		<< " Personas sanas total " << healthy_people << ", promedio " << healthy_people / actual_tic << ", porcentaje " << number_people * healthy_people / 100 << endl
-		<< " Personas enfermas total " << sick_people << ", promedio " << sick_people / actual_tic << ", porcentaje " << number_people * sick_people / 100 << endl
-		<< " Personas inmunes total " << inmune_people << ", promedio " << inmune_people / actual_tic << ", porcentaje " << number_people * inmune_people / 100 << endl;
+#pragma omp single
+	{
+		ofstream file;
+		file.open(name, ios_base::app);
+		file << "Reporte del tic " << actual_tic << endl
+			<< " Personas muertas total " << dead_people << ", promedio " << dead_people / actual_tic << ", porcentaje " << number_people * dead_people / 100 << endl
+			<< " Personas sanas total " << healthy_people << ", promedio " << healthy_people / actual_tic << ", porcentaje " << number_people * healthy_people / 100 << endl
+			<< " Personas enfermas total " << sick_people << ", promedio " << sick_people / actual_tic << ", porcentaje " << number_people * sick_people / 100 << endl
+			<< " Personas inmunes total " << inmune_people << ", promedio " << inmune_people / actual_tic << ", porcentaje " << number_people * inmune_people / 100 << endl;
 
-	file.close();//Hacer archivo
+		file.close();//Hacer archivo
+	}
 
 	while (it != lists.end()) {
 		i = (*it).getX();
