c393f0c22cad36f817f920d37a4bbe6bad15e33e
Dess96 <dessalfaro@gmail.com>
2018-10-16 15:00:26 -0600

    Version 4
    *Nueva forma de hacer lineas

--- Proyecto Grafica/test.cpp
+++ Proyecto Grafica/test.cpp
@@ -1,75 +1,109 @@
+#include<iostream>
+#include<string>
+#include<omp.h>
+#include<thread>
+#include<chrono>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
 #include <GL/glew.h>
 #include <GL/glut.h>
+#include"Person.h"
+#include"Simulator.h"
 
-GLuint program;
-GLint attribute_coord2d;
-GLint uniform_offset_x;
-GLint uniform_scale_x;
-GLint uniform_sprite;
+using namespace std;
+using namespace std::chrono;
 
-float offset_x = 0.0;
-float scale_x = 1.0;
+#define _CRT_SECURE_NO_WARNINGS 1 // para deshabilitar errores por uso de funciones deprecated sobre CRT o consola
+#pragma warning(disable : 4996)
 
-struct point {
-	GLfloat x;
-	GLfloat y;
-};
+bool validateProb(double, double);
+bool validatePeople(int);
+void display();
 
-GLuint vbo;
+int main(int argc, char *argv[]) {
+	unsigned n = std::thread::hardware_concurrency(); //Saca la cantidad de nucleos en la computadora
+	int thread_countM = 2 * n;
+	int world_sizeM, death_durationM, ticM, number_peopleM, healthy_people;
+	int new_sim = 1;
+	int sims = 1;
+	double infectedM, infectiousnessM, chance_recoverM, arch_time;
+	string number;
+	string name = " ";
+	Simulator sim;
+	do {
+		infectiousnessM = -1;
+		chance_recoverM = -1;
+		number_peopleM = -1;
+		while (!validateProb(infectiousnessM, chance_recoverM) || !validatePeople(number_peopleM)) { //Pedir y validar datos
+			cout << "Ingrese el numero de personas en el mundo (de 0 a 10 000)" << endl;
+			cin >> number_peopleM;
+			cout << "Ingrese la potencia infecciosa del mundo (decimal entre 0 y 1)" << endl;
+			cin >> infectiousnessM;
+			cout << "Ingrese la probabilidad de recuperacion (decimal entre 0 y 1)" << endl;
+			cin >> chance_recoverM;
+			cout << "Ingrese el tiempo maximo que puede vivir una persona infectada" << endl;
+			cin >> death_durationM;
+			cout << "Ingrese el porcentaje de personas infectadas inicialmente" << endl;
+			cin >> infectedM;
+			cout << "Ingrese el tamano del espacio bidimensional" << endl;
+			cin >> world_sizeM;
+			cout << "Ingrese la cantidad de tics" << endl;
+			cin >> ticM;
+		}
+		cout << "Se usaran " << thread_countM << " hilos en esta simulacion" << endl;
+		name = "report_"; //Nos encargamos de crear el nombre del futuro archivo por simulacion
+		number = to_string(sims);
+		name.append(number);
+		name.append(".txt");
+		healthy_people = sim.initialize(number_peopleM, infectiousnessM, chance_recoverM, death_durationM, infectedM, world_sizeM, ticM, thread_countM); //Metodo inicializador
+		arch_time = sim.update(name, healthy_people); //Metodo que actualiza el mundo por tic
+		//GRAFICACION
+		glutInit(&argc, argv);
+		glutInitDisplayMode(GLUT_RGB);
+		glutInitWindowSize(640, 480);
+		glutCreateWindow("Data");
+		glutDisplayFunc(display);
+		glutMainLoop();
+		//sim.init_resources();
+		cout << endl;
+		cout << "Desea ver otra simulacion?" << endl;
+		cout << "1. Si   2. No" << endl;
+		cin >> new_sim;
+		sims++;
+		name = " ";
+	} while (new_sim == 1);
+	cin >> number_peopleM; //Para que no se cierre
+	return 0;
+}
 
-int init_resources() {
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	point graph[2000];
-	for (int i = 0; i < 2000; i++) {
-		float x = (i - 1000.0) / 100.0;
+bool validateProb(double infectiousness, double chance_recover) {
+	return infectiousness >= 0 && infectiousness <= 1 && chance_recover >= 0 && chance_recover <= 1;
+}
 
-		graph[i].x = x;
-		graph[i].y = sin(x * 10.0) / (1.0 + x * x);
-	}
-	glBufferData(GL_ARRAY_BUFFER, sizeof graph, graph, GL_STATIC_DRAW);
-
-	return 1;
+bool validatePeople(int number_people) {
+	return number_people >= 0 && number_people <= 100000;
 }
 
 void display() {
-	glUseProgram(program);
-
-	glUniform1f(uniform_offset_x, offset_x);
-	glUniform1f(uniform_scale_x, scale_x);
-
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-	glEnableVertexAttribArray(attribute_coord2d);
-	glVertexAttribPointer(attribute_coord2d, 2, GL_FLOAT, GL_FALSE, 0, 0);
-	glUniform1f(uniform_sprite, 0);
-	glDrawArrays(GL_LINE_STRIP, 0, 2000);
-	glutSwapBuffers();
-}
-
-void free_resources() {
-	glDeleteProgram(program);
-}
-
-int main(int argc, char *argv[]) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB);
-	glutInitWindowSize(640, 480);
-	glutCreateWindow("Data");
-
-	GLenum glew_status = glewInit();
-
-	if (init_resources()) {
-		glutDisplayFunc(display);
-		glutMainLoop();
+	double msg;
+	double coordx = -1.0;
+	double coordy = -1.0;
+	int rank;
+	int i = 0;
+	glBegin(GL_LINE_STRIP);
+	glColor3f(0.0, 1.0, 0.0);
+	glLineWidth(2);
+	glVertex2f(-1.0, -1.0);
+	while (i < 20) {
+		coordx += 0.3;
+		coordy += 0.1;
+		glVertex2f(coordx, coordy);
+		i++;
 	}
-	free_resources();
-	return 0;
+	glEnd();
+	glFlush();
+	glutSwapBuffers();
 }
