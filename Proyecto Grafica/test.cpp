5cd5eecfb79f2fe70db2c9e21829bbd1818e3cb1
Dess96 <dessalfaro@gmail.com>
2018-10-15 19:48:34 -0600

    Version 3
    *Codigo que hace una grafica

--- Proyecto Grafica/test.cpp
+++ Proyecto Grafica/test.cpp
@@ -1,138 +1,75 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-#include <iostream>
+#include <GL/glew.h>
+#include <GL/glut.h>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
+GLuint program;
+GLint attribute_coord2d;
+GLint uniform_offset_x;
+GLint uniform_scale_x;
+GLint uniform_sprite;
 
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-const char *vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColor;\n"
-"out vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
-"   ourColor = aColor;\n"
-"}\0";
-
-const char *fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(ourColor, 1.0f);\n"
-"}\n\0";
+float offset_x = 0.0;
+float scale_x = 1.0;
 
 struct point {
 	GLfloat x;
 	GLfloat y;
-	GLfloat z;
-	GLfloat R;
-	GLfloat G;
-	GLfloat B;
 };
 
-int main(){
-	//VENTANA 
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	//VENTANA
+GLuint vbo;
 
-	//GLAD
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Fallo en GLAD" << std::endl;
-		return -1;
-	}
-	//GLAD
+int init_resources() {
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	point graph[2000];
+	for (int i = 0; i < 2000; i++) {
+		float x = (i - 1000.0) / 100.0;
 
-	point graph[4000];
-	double x = -1.0;
-	double y = 0.1;
-	for (int i = 0; i < 4000; i++) {
 		graph[i].x = x;
-		graph[i].y = y;
-		graph[i].z = 0.0;
-		graph[i].R = 0.0;
-		graph[i].G = 0.0;
-		graph[i].B = 1.0;
-		x+= 10;
-		y++;
+		graph[i].y = sin(x * 10.0) / (1.0 + x * x);
 	}
-	//VERTEX POSICIONES X, Y, Z, W
-	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
+	glBufferData(GL_ARRAY_BUFFER, sizeof graph, graph, GL_STATIC_DRAW);
 
-	//FRAGMENT COLORES RGBA (opacidad)
-	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
+	return 1;
+}
 
-	//JUNTAR LOS SHEDERS EN UN NUEVO OBJETO
-	int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
+void display() {
+	glUseProgram(program);
 
-	//BORRAR OBJETOS
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-	
-	//BUFFERS
-	unsigned int VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(graph), graph, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(graph), graph, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	glUseProgram(shaderProgram);
+	glUniform1f(uniform_offset_x, offset_x);
+	glUniform1f(uniform_scale_x, scale_x);
 
-	//RENDERIZADO
-	while (!glfwWindowShouldClose(window)){
-		processInput(window);
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		// PRIMER TRIANGULO
-		glBindVertexArray(VAO); 
-		glDrawArrays(GL_LINE_STRIP, 0, 3);
-	//	glDrawElements(GL_LINE_STRIP, 6, GL_UNSIGNED_INT, 0);
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	glEnableVertexAttribArray(attribute_coord2d);
+	glVertexAttribPointer(attribute_coord2d, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glUniform1f(uniform_sprite, 0);
+	glDrawArrays(GL_LINE_STRIP, 0, 2000);
+	glutSwapBuffers();
+}
+
+void free_resources() {
+	glDeleteProgram(program);
+}
+
+int main(int argc, char *argv[]) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB);
+	glutInitWindowSize(640, 480);
+	glutCreateWindow("Data");
+
+	GLenum glew_status = glewInit();
+
+	if (init_resources()) {
+		glutDisplayFunc(display);
+		glutMainLoop();
 	}
-
-	//BORRAR BUFFERS
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
-	glfwTerminate();
+	free_resources();
 	return 0;
-}
-
-//Salir con el ESC
-void processInput(GLFWwindow *window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-}
-
-//Resize de ventana
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
 }
